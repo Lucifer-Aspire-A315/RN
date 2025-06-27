05d07c1b1d951dcb4422f08927ca461b62601b5a
rn fintech <fintechrn@gmail.com>
2025-06-27 11:38:23 +0000

    my real domain is https://rnfintech.com and i have a redirected www.rnfi

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.rnfintech.com';
+  const baseUrl = 'https://rnfintech.com';
 
   return [
     {
